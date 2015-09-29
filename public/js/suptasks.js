312c4bdf968b6a162b7fa33323ab30fe92d6b1be
Premysl Donat <donat@uol.cz>
2015-09-29 20:44:28 +0200

    Refactor JS a bit

--- public/js/suptasks.js
+++ public/js/suptasks.js
@@ -1,30 +1,26 @@
 var Suptasks = {
-  timer: 0
+  timer: 0,
+
+  incrementTimer: function() { 
+    Suptasks.timer = Suptasks.timer + 1; 
+  },
+
+  updateView: function() {
+    var inputs = document.getElementsByClassName('timer-inputs');
+    for (i = 0; i < inputs.length; i++) {
+      inputs[i].value = Suptasks.timer;
+    };
+  },
+
+  startStopWatch: function() {
+    stopWatch = window.setInterval(function() { Suptasks.incrementTimer(); Suptasks.updateView() }, 6000);
+  },
+
+  stopStopWatch: function() {
+    window.clearInterval(stopWatch);
+  }
 };
 
-Suptasks.incrementTimer = function() {
-  Suptasks.timer = Suptasks.timer + 1;
-};
-
-Suptasks.updateView = function() {
-  var inputs = document.getElementsByClassName('timer-inputs');
-  for (i = 0; i < inputs.length; i++) {
-    inputs[i].value = Suptasks.timer;
-  };
-
-  var tags = document.getElementsByClassName('timer-tags');
-  for (i = 0; i < tags.length; i++) {
-    tags[i].innerHTML = Suptasks.timer;
-  };
-};
-
-Suptasks.startStopWatch = function() {
-  stopWatch = window.setInterval(function() { Suptasks.incrementTimer(); Suptasks.updateView() }, 60000);
-};
-
-Suptasks.stopStopWatch = function() {
-  window.clearInterval(stopWatch);
-};
 
 window.onload = function() {
   Suptasks.startStopWatch();
