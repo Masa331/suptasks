e9d52f04c0dfdbc1fcea0435c0c6356d4a16a63a
Premysl Donat <donat@uol.cz>
2015-10-14 19:39:56 +0200

    Remove obsolete files

--- database/migrations/1_create_tasks.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE tasks (id INTEGER NOT NULL PRIMARY KEY,
-                    description VARCHAR NOT NULL,
-                    time_cost INTEGER,
-                    completed BOOLEAN NOT NULL DEFAULT 0 CHECK (completed IN (0,1)),
-                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL);
