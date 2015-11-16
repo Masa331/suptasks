23ff12248e2d3488ed523fa1109ffdd8218c038d
Premysl Donat <pdonat@seznam.cz>
2015-11-16 20:49:40 +0100

    Don't fail if no time records present
    Time records index page couldn't be accessed when no time records
    existed. So eg. new user would fail everytime visiting time records
    prior to creating any.

--- lib/time_records_pager.rb
+++ lib/time_records_pager.rb
@@ -16,17 +16,9 @@ class TimeRecordsPager
   #
   # @return array of TimeRecords [Array]
   def by_number_of_days(number_of_days)
+    page_count = page_count_for_number_of_days(number_of_days)
 
-    number_of_pages =
-      begin
-        oldest_record = time_records.order(:started_at).first
-        days_from_first_record = (Date.today - oldest_record.started_at.to_date).to_i
-
-        pn = (days_from_first_record/number_of_days.to_f).ceil
-        pn == 0 ? 1 : pn
-      end
-
-    1.upto(number_of_pages).map do |page_number|
+    1.upto(page_count).map do |page_number|
       start_date = Date.today - (number_of_days * page_number)
       # HACK: +1 cos Date.today.to_time returns eg. 1.1.2015 00:00 but we want in fact 23:59 and
       #    00:00 next day is almost the same
@@ -34,5 +26,15 @@ class TimeRecordsPager
 
       TimeRecords.new(time_records.where('started_at >= ? ', start_date).where('started_at <= ? ', end_date))
     end
+  end
+
+  def page_count_for_number_of_days(number_of_days)
+    oldest_record = time_records.order(:started_at).first
+
+    return 1 if oldest_record.nil? || oldest_record.started_at.to_date == Date.today
+
+    days_from_first_record = (Date.today - oldest_record.started_at.to_date).to_i
+
+    (days_from_first_record/number_of_days.to_f).ceil
   end
 end
---
+++ test/time_records_pager_test.rb
@@ -0,0 +1,20 @@
+require 'sequel'
+
+DB = Sequel.sqlite
+Sequel.extension :migration
+Sequel::Migrator.run(DB, 'db/migrations')
+
+require_relative "test_helper"
+require_relative "../lib/time_records_pager"
+require_relative "../lib/time_record"
+require_relative "../lib/time_records"
+
+class TimeRecordsPagerTest < Minitest::Test
+  def test_at_least_one_page_is_returned
+    pager = TimeRecordsPager.new(TimeRecord.select_all)
+    paged = pager.by_number_of_days(1)
+
+    assert_instance_of Array, paged
+    assert_instance_of TimeRecords, paged.first
+  end
+end
