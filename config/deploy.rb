9b7c223580d3af1c583443be249d26a99d545249
Premysl Donat <pdonat@seznam.cz>
2016-04-17 12:34:17 +0200

    Update capistrano deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 set :application, 'suptasks'
 set :repo_url, 'git@github.com:Masa331/suptasks.git'
 set :deploy_to,   '/var/www/suptasks'
+set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets db/task_databases/databases}
+set :linked_files, %w{.production suptasks.log db/user_database/user_database}
 set :pty, true
 
 namespace :deploy do
